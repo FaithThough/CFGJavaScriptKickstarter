48d60023d4f99ad4b029e92e17fba5c8b3bf6a7a
FaithThough <faithphillips96@gmail.com>
2023-11-20 14:16:33 +0000

    JS and HTML changes
    - Surprise me button that generates a time between 1 and 10 mins using math random
    - Variable to track timer status that prevents the user from starting the timer multiple times

--- project.js
+++ project.js
@@ -1,8 +1,20 @@
 // Declare a variable to store the interval ID for the timer
 let intervalId;
 
+// Declare a variable to track the timer status
+let isTimerRunning = false;
+
 // Function to start the relaxation timer
 function startTimer() {
+  // Check if the timer is already running
+  if (isTimerRunning) {
+    alert(
+      "Timer is already running. Please wait until it finishes or clear it."
+    );
+    return;
+  }
+  // Set isTimerRunning to true to indicate that the timer is in progress
+  isTimerRunning = true;
   // Get the user input for the timer duration
   const inputTime = document.getElementById("inputTime").value;
 
@@ -13,7 +25,6 @@ function startTimer() {
   }
 
   console.log(`Relaxation Timer started for ${inputTime} ${minutes}`); //
-
 
   // Define constants for time units in milliseconds
   const second = 1000,
@@ -52,6 +63,8 @@ function startTimer() {
       document.getElementById("countdown").style.display = "none";
       document.getElementById("content").style.display = "block";
       clearInterval(intervalId); // Clear the interval
+      // Set isTimerRunning back to false when the timer completes
+      isTimerRunning = false;
     }
   }, 1000); // Update every second
 }
@@ -60,6 +73,8 @@ function startTimer() {
 function clearTimer() {
   // Clear the interval to stop the timer
   clearInterval(intervalId);
+  // Set isTimerRunning back to false
+  isTimerRunning = false;
   // Hide the countdown and content divs
   document.getElementById("countdown").style.display = "none";
   document.getElementById("content").style.display = "none";
@@ -67,3 +82,28 @@ function clearTimer() {
   document.getElementById("headline").innerText = "Relaxation Timer";
   // Clear the input field
   document.getElementById("inputTime").value = "";
+}
+
+//Function for the surprise me button
+function surpriseMe() {
+  // Check if the timer is already running
+  if (isTimerRunning) {
+    alert(
+      "Timer is already running. Please wait until it finishes or clear it."
+    );
+    return;
+  }
+  // Generate a random time
+  // Minimum time is one minute
+  const min = 1;
+  // Maxiumum time is ten minutes
+  const max = 10;
+  const randomInteger = Math.floor(Math.random() * (max - min + 1)) + min;
+  console.log(randomInteger);
+  //The user is informed of the random integer generated
+  alert(`The random time generated is ${randomInteger} minutes`);
+  // The input value is set to the random time
+  document.getElementById("inputTime").value = randomInteger;
+  //The timer is immediately started
+  startTimer();
+}
